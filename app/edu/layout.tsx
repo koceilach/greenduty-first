869b7430b15e9a71a0919f9c275ae867884e8098
koceilach <157543173+koceilach@users.noreply.github.com>
2026-02-10 22:01:06 +0100

    Fix:deleted edu

--- app/edu/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "@/components/theme-provider";
-
-export default function EduLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      {children}
-    </ThemeProvider>
-  );
-}
