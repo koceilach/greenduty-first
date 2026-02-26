ddcef254fc8a5e028432e1ba41c197b7caa34cec
koceilach <157543173+koceilach@users.noreply.github.com>
2026-02-26 13:40:28 +0100

    essay

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,16 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
 export const dynamic = "force-dynamic";
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
+export const authOptions: NextAuthOptions = {
+providers: [
+GoogleProvider({
+clientId: process.env.GOOGLE_CLIENT_ID as string,
+clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+}),
+],
+secret: process.env.NEXTAUTH_SECRET as string,
+debug: true,
 };
 
 const handler = NextAuth(authOptions);
